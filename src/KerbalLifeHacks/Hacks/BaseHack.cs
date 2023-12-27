ffd52dc9e72cfb5a46810b52ed1cd760221244e8
Jan Bureš <jan.bures@live.com>
2023-12-28 00:29:15 +0100

    Basic structure of mod finished

---
+++ src/KerbalLifeHacks/Hacks/BaseHack.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx.Configuration;
+using HarmonyLib;
+using KerbalLifeHacks.Config;
+using KSP.Game;
+using SpaceWarp.API.Logging;
+
+namespace KerbalLifeHacks.Hacks;
+
+/// <summary>
+/// Base class for all hacks.
+/// </summary>
+public abstract class BaseHack : KerbalMonoBehaviour
+{
+    private Configuration Config { get; }
+
+    /// <summary>
+    /// The logger for the hack.
+    /// </summary>
+    internal ILogger Logger { get; }
+
+    /// <summary>
+    /// The harmony instance for the hack.
+    /// </summary>
+    private protected Harmony HarmonyInstance { get; }
+
+    /// <summary>
+    /// Creates a new hack.
+    /// </summary>
+    private protected BaseHack()
+    {
+        Logger = new BepInExLogger(BepInEx.Logging.Logger.CreateLogSource($"KLH/{GetType().Name}"));
+        Config = KerbalLifeHacksPlugin.Config;
+        HarmonyInstance = new Harmony(GetType().FullName);
+    }
+
+    private protected ConfigEntry<T> BindConfigValue<T>(string key, T defaultValue, string description = null)
+    {
+        return Config.BindHackValue(GetType(), key, defaultValue, description);
+    }
+
+    /// <summary>
+    /// Called when the hack is initialized.
+    /// </summary>
+    public virtual void OnInitialized()
+    {
+    }
+}
--- src/KerbalLifeHacks/KerbalLifeHacksPlugin.cs
+++ src/KerbalLifeHacks/KerbalLifeHacksPlugin.cs
@@ -1,6 +1,9 @@
 using BepInEx;
 using JetBrains.Annotations;
+using KerbalLifeHacks.Config;
+using KerbalLifeHacks.Hacks;
 using SpaceWarp;
+using SpaceWarp.API.Configuration;
 using SpaceWarp.API.Mods;
 
 namespace KerbalLifeHacks;
@@ -27,17 +30,71 @@ public class KerbalLifeHacksPlugin : BaseSpaceWarpPlugin
     /// </summary>
     [PublicAPI] public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
 
-    /// <summary>
-    /// A singleton instance of the mod.
-    /// </summary>
-    [PublicAPI] public static KerbalLifeHacksPlugin Instance { get; set; }
+    internal new static Configuration Config;
+
+    private readonly List<BaseHack> _hacks = new();
+
+    public override void OnPreInitialized()
+    {
+        Type[] types;
+        try
+        {
+            types = typeof(KerbalLifeHacksPlugin).Assembly.GetTypes();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Could not get types: ${ex.Message}");
+            return;
+        }
+
+        Config = new Configuration(base.Config);
+
+        foreach (var type in types)
+        {
+            if (type.IsAbstract || !type.IsSubclassOf(typeof(BaseHack)))
+            {
+                continue;
+            }
+
+            try
+            {
+                var isLoaded = LoadHack(type);
+                Logger.LogInfo($"Hack {type.Name} is " + (isLoaded ? "enabled" : "disabled"));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"Error loading hack {type.FullName}: {ex}");
+            }
+        }
+    }
+
+    private bool LoadHack(Type type)
+    {
+        if (!Config.IsHackEnabled(type))
+        {
+            return false;
+        }
+
+        var hack = gameObject.AddComponent(type) as BaseHack;
+        if (hack == null)
+        {
+            throw new Exception($"Could not instantiate hack {type.Name}.");
+        }
+
+        hack.transform.parent = transform;
+        _hacks.Add(hack);
+
+        return true;
+    }
 
     /// <summary>
     /// Runs when the mod is first initialized.
     /// </summary>
     public override void OnInitialized()
     {
-        base.OnInitialized();
-        Instance = this;
+        foreach (var hack in _hacks)
+        {
+            hack.OnInitialized();
+        }
     }
 }
