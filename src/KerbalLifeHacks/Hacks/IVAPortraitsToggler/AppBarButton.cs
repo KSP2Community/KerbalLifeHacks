3a64c4f1bd0667ff601188c8f718e0f0997a06d3
Jan Bureš <jan.bures@live.com>
2024-01-22 22:12:45 +0100

    Some small refactoring

--- src/KerbalLifeHacks/Hacks/IVAPortraitsToggler/AppBarButton.cs
+++ src/KerbalLifeHacks/Hacks/IVAPortraitsToggler/AppBarButton.cs
@@ -5,55 +5,71 @@ using SpaceWarp.API.UI.Appbar;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityObject = UnityEngine.Object;
+
 namespace KerbalLifeHacks.Hacks.IVAPortraitsToggler;
 
 internal class AppBarButton
 {
-    private UIValue_WriteBool_Toggle _buttonState;
-    public GameObject AddButton(string buttonTooltip, string buttonId, Texture2D buttonIcon, Action<bool> function, int siblingIndex = -1)
+    private readonly UIValue_WriteBool_Toggle _buttonState;
+
+    public AppBarButton(
+        string buttonTooltip,
+        string buttonId,
+        Texture2D buttonIcon,
+        Action<bool> function,
+        int siblingIndex = -1
+    )
     {
-        // Find 'ButtonBar' gameobject
-        var list = GameObject.Find("GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Popup Canvas/Container/ButtonBar");
+        // Find 'ButtonBar' game object
+        var list = GameObject.Find(
+            "GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Popup Canvas/Container/ButtonBar"
+        );
+
         // Get 'NonStageable-Resources' button
         var nonStageableResources = list != null ? list.GetChild("BTN-NonStageable-Resources") : null;
-        
+
         if (list == null || nonStageableResources == null)
         {
-            return null;
+            return;
         }
-        
-        // Clone 'NonStageable-Resources' button.
-        
+
+        // Clone 'NonStageable-Resources' button
+
         var barButton = UnityObject.Instantiate(nonStageableResources, list.transform);
-        if(siblingIndex >= 0 && siblingIndex < list.transform.childCount-1)
+        if (siblingIndex >= 0 && siblingIndex < list.transform.childCount - 1)
+        {
             barButton.transform.SetSiblingIndex(siblingIndex);
-        
+        }
+
         barButton.name = buttonId;
-        
-        // Change the tooltip.
+
+        // Change the tooltip
         barButton.GetComponent<BasicTextTooltipData>()._tooltipTitleKey = buttonTooltip;
-        
-        // Change the icon.
+
+        // Change the icon
         var sprite = Appbar.GetAppBarIconFromTexture(buttonIcon);
         var icon = barButton.GetChild("Content").GetChild("GRP-icon");
         var image = icon.GetChild("ICO-asset").GetComponent<Image>();
         image.sprite = sprite;
 
-        // Add our function call to the toggle.
-        var utoggle = barButton.GetComponent<ToggleExtended>();
-        utoggle.onValueChanged.AddListener(state => function(state));
-        utoggle.onValueChanged.AddListener(SetButtonState);
-        
-        // Set the initial state of the button.
+        // Add our function call to the toggle
+        var toggle = barButton.GetComponent<ToggleExtended>();
+        toggle.onValueChanged.AddListener(state => function(state));
+        toggle.onValueChanged.AddListener(SetButtonState);
+
+        // Set the initial state of the button
         _buttonState = barButton.GetComponent<UIValue_WriteBool_Toggle>();
         _buttonState.valueKey = $"Is{buttonId}Visible";
         _buttonState.BindValue(new Property<bool>(false));
-        return barButton;
     }
-    
+
     public void SetButtonState(bool state)
     {
-        if (_buttonState == null) return;
+        if (_buttonState == null)
+        {
+            return;
+        }
+
         _buttonState.SetValue(state);
     }
 }
--- src/KerbalLifeHacks/Hacks/IVAPortraitsToggler/IVAPortraitsToggler.cs
+++ src/KerbalLifeHacks/Hacks/IVAPortraitsToggler/IVAPortraitsToggler.cs
@@ -1,35 +1,43 @@
 ﻿using KSP.Game;
 using KSP.Messages;
-using KSP.Sim.impl;
 using SpaceWarp.API.Assets;
 using UnityEngine;
+
 namespace KerbalLifeHacks.Hacks.IVAPortraitsToggler;
 
 [Hack("IVA Portraits Toggler")]
 public class IVAPortraitsToggler : BaseHack
 {
     private AppBarButton _buttonBar;
+
+    // ReSharper disable once InconsistentNaming, IdentifierTypo
     private Canvas _ivaportraits_canvas;
-    
+
     public override void OnInitialized()
     {
         Messages.PersistentSubscribe<FlightViewEnteredMessage>(OnFlightViewEnteredMessage);
         Messages.PersistentSubscribe<VesselChangedMessage>(OnVesselChangedMessage);
     }
-    
+
     private void OnFlightViewEnteredMessage(MessageCenterMessage msg)
     {
         if (_ivaportraits_canvas == null)
         {
             var instruments = GameManager.Instance.Game.UI.FlightHud._instruments;
+            // ReSharper disable once StringLiteralTypo
             instruments.TryGetValue("group_ivaportraits", out var ivaPortraits);
-            if(ivaPortraits != null)
+            if (ivaPortraits != null)
+            {
                 _ivaportraits_canvas = ivaPortraits._parentCanvas;
+            }
         }
 
-        if (_buttonBar != null) return;
-        _buttonBar = new AppBarButton();
-        _buttonBar.AddButton(
+        if (_buttonBar != null)
+        {
+            return;
+        }
+
+        _buttonBar = new AppBarButton(
             "IVA Portraits",
             "BTN-IVA-Portraits",
             AssetManager.GetAsset<Texture2D>($"KerbalLifeHacks/images/IVAPortraitsToggler-icon.png"),
@@ -37,17 +45,23 @@ public class IVAPortraitsToggler : BaseHack
             0
         );
     }
-    
+
     private void OnVesselChangedMessage(MessageCenterMessage msg)
     {
-        if (msg is not VesselChangedMessage { Vessel: not null } message) return;
-        IGGuid vesselGuid = message.Vessel.GetControlOwner().GlobalId;
-        var allKerbalsInSimObject = GameManager.Instance.Game.KerbalManager._kerbalRosterManager?.GetAllKerbalsInSimObject(vesselGuid);
+        if (msg is not VesselChangedMessage { Vessel: { } vessel })
+        {
+            return;
+        }
+
+        var vesselGuid = vessel.GetControlOwner().GlobalId;
+        var allKerbalsInSimObject = GameManager.Instance.Game.KerbalManager._kerbalRosterManager
+            ?.GetAllKerbalsInSimObject(vesselGuid);
         var state = allKerbalsInSimObject?.Count > 0;
+
         ToggleIVAPortraitsCanvas(state);
         _buttonBar.SetButtonState(state);
     }
-    
+
     public void ToggleIVAPortraitsCanvas(bool state)
     {
         _ivaportraits_canvas.enabled = state;
